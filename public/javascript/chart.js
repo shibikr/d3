d087afcb047a6670552cb4eecb33e3f67a183e7a
shibi <shibidewdrops7@gmail.com>
2016-10-27 13:18:33 +0530

    created bar chart for 10 random numbers

--- public/javascript/chart.js
+++ public/javascript/chart.js
@@ -6,7 +6,7 @@ var createRandomNumbers = function(){
     numbers.push(randomNumber);
   }
   return numbers;
-}
+};
 
 const WIDTH = 1000;
 const HEIGHT = 750;
@@ -19,18 +19,18 @@ var translate = function(x, y){
 	return "translate("+x+","+y+")";
 };
 
-var loadChart = function(){
-  var svg = d3.select('.container').append('svg')
+var xScale = d3.scaleLinear()
+    .domain([0,10])
+    .range([0, INNER_WIDTH]);
+
+var yScale = d3.scaleLinear()
+    .domain([0,100])
+    .range([INNER_HEIGHT, 0]);
+
+var createChart = function(idName){
+  var svg = d3.select('#'+idName).append('svg')
               .attr('width',WIDTH)
               .attr('height',HEIGHT);
-
-  var xScale = d3.scaleLinear()
-	    .domain([0,10])
-	    .range([0, INNER_WIDTH]);
-
-	var yScale = d3.scaleLinear()
-	    .domain([0,100])
-	    .range([INNER_HEIGHT, 0]);
 
 	var xAxis = d3.axisBottom(xScale).ticks(10);
 	var yAxis = d3.axisLeft(yScale).ticks(10);
@@ -46,24 +46,39 @@ var loadChart = function(){
 		.classed('yAxis', true);
 
 	var g = svg.append('g')
-		.attr('transform',  translate(MARGIN, MARGIN));
+		.attr('transform',  translate(MARGIN, MARGIN))
+    .classed(idName,true);
+};
 
-  var drawLine = function(){
-    var data = createRandomNumbers();
-    var line = d3.line()
-      .x(function(d,i){return xScale(i+1)})
-      .y(function(d){return yScale(d)});
+var drawLine = function(data,idName){
+  var line = d3.line()
+    .x(function(d,i){return xScale(i)})
+    .y(function(d){return yScale(d)});
+  d3.select('.'+idName).append('path')
+    .classed('random-numbers', true)
+    .attr('d', line(data));
+};
 
-    g.append('path')
-      .classed('random-numbers', true)
-      .attr('d', line(data));
+var drawBar = function(data,idName){
+    d3.select('.'+idName).selectAll('rect')
+      .data(data)
+      .enter().append('rect')
+      .attr('x',function(d,i){return xScale(i+1)-20})
+      .attr('y',function(d){return yScale(d)})
+      .attr('width',20)
+      .attr('height',function(d){return INNER_HEIGHT - yScale(d)});
+};
 
-  };
-
+var loadChart = function(){
+  createChart('line-chart');
+  createChart('bar-chart');
   setInterval(function(){
-    g.selectAll('path').remove();
-    drawLine();
+    var data = createRandomNumbers();
+    d3.select('.line-chart').selectAll('path').remove();
+    drawLine(data,'line-chart');
+    d3.select('.bar-chart').selectAll('rect').remove();
+    drawBar(data,'bar-chart');
   }, 250);
-}
+};
 
 window.onload = loadChart;
